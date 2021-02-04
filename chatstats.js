7583f97da4e4f8597c5deab25722f067f020234e
Kiraseira <kiraseira@protonmail.com>
2021-02-04 22:36:03 +0100

    new chatstats command

---
+++ chatstats.js
@@ -0,0 +1,30 @@
+exports.kirabot_command = {
+		name: "chatstats",
+		desc: "statistics about the chatters of a stream",
+		help: "shows chat statistics about a specified channel",
+		aliases: null,
+		userlevel: 0,
+		pingsender: 1,
+		execution_check: true,
+		cds: {
+				user: 40,				//how often a user can call this command (in secs)
+				channel: 30 				//how often the command can be used by anyone
+			},
+		code: function(sender, lparam) {
+				return new Promise((resolve, reject) => {
+					//Example {resolvedOnSucces: true, msg: "successfully played the sound." }
+					const inmsg = lparam.split(" ");
+					if (inmsg.length<2){
+						resolve({resolvedOnSucces: false, msg: "please specify a channel name." });
+						return;
+					}
+					ksb.got.get("https://tmi.twitch.tv/group/user/"+inmsg[1]+"/chatters").json().then((data) => {
+						resolve({resolvedOnSucces: true, msg: `VIPS: ${data.chatters.vips.length}, MODS : ${data.chatters.moderators.length}, viewers ${data.chatters.viewers.length}`});
+						return;
+					}).catch((err) => {
+						reject(err);
+						return;
+					});
+				});
+				}
+}
