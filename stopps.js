16db46a1c4d2b996fe7e25ee89985a6fe25d71ba
Kiraseira <kiraseira@protonmail.com>
2021-02-16 12:16:06 +0100

    stopps now reset the execution cooldown of playsound command

--- stopps.js
+++ stopps.js
@@ -15,6 +15,12 @@ exports.kirabot_command = {
 					ksb.player.stop();
 					ksb.util.logger(3, "Stopping playback on command.");
 					ksb.status = "idle";
+					//reset the execution status of the playsound command so the user doesn't have to wait
+					//for the 1 min grace period to be over to use the command again.
+					if(ksb.util.getExecutionStatus(sender, "playsound")){
+						let i = ksb.util.cooldowns.findIndex(nam => nam.usr === sender && nam.cmd === "__command_execution:playsound");
+						if(i!=-1) ksb.util.cooldowns[i].ptime -= 61;
+					}
 					resolve({resolvedOnSuccess: true, msg: "Playback stopped."});
 				});
 				}
